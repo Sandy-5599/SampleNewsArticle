103b0ea7a855663467294bb09ed74619b04062d6
SANDHYA\Me <sandhya.minupala@gmail.com>
2023-07-02 15:46:12 +1000

    Mock test updated

--- app/src/main/java/com/example/samplenewsarticle/repository/NewsArticleRepository.kt
+++ app/src/main/java/com/example/samplenewsarticle/repository/NewsArticleRepository.kt
@@ -72,6 +72,7 @@ val sampleMock =
 	"onTime": 1425955024417,
 	"sponsored": false,
 	"displayName": "AFR iPad Editor's Choice",
+ 
 	"assets": [{
 		"id": 1520675543,
 		"categories": [{
@@ -186,16 +187,127 @@ val sampleMock =
 			"overrideHeadline": "Housing pipeline shrinks as apartment approvals plummet",
 			"overrideAbstract": "Economic headwinds are hitting Australia’s largest cities, as they try to boost housing development to overcome a long-standing shortage of homes."
 		},
+		"timeStamp": 1686429492000
+	},
+{
+		"id": 1520675543,
+		"categories": [{
+			"name": "Residential",
+			"sectionPath": "/real-estate/residential",
+			"orderNum": 0
+		}],
+		"authors": [{
+			"name": "Samantha Hutchinson",
+			"title": "Reporter",
+			"email": "samantha.hutchinson@smh.com.au",
+			"relatedAssets": [],
+			"relatedImages": []
+		}],
+		"url": "http://www.afr.com/real-estate/residential/count-on-two-years-to-get-a-bondi-apartment-approved-20230531-p5dcrb",
+		"lastModified": 1685526714038,
+		"sponsored": false,
+		"headline": "Count on two years to get a Bondi apartment approved",
+		"indexHeadline": "Count on two years to get a Bondi apartment approved",
+		"tabletHeadline": "",
+		"theAbstract": "Sydney builder Alistair Jones says the approval process is taking time and money and reducing the viability of medium density projects that would deliver much-needed housing supply.",
+		"byLine": "Samantha Hutchinson",
+		"acceptComments": false,
+		"numberOfComments": 0,
+		"relatedAssets": [],
+		"relatedImages": [{
+			"id": 1031856532,
+			"categories": [],
+			"brands": [],
+			"authors": [],
+			"url": "https://www.fairfaxstatic.com.au/content/dam/images/h/2/c/9/l/g/image.related.afrArticleLead.1536x1010.p5dcrb.13zzqx.png/1685602003603.jpg",
+			"lastModified": 1685602003603,
+			"sponsored": false,
+			"description": "Builder Alistair Jones has waited 18 months to get approval to convert this freestanding home into a duplex. ",
+			"photographer": "Dominic Lorrimer",
+			"type": "afrArticleLead",
+			"width": 230,
+			"height": 540,
+			"assetType": "IMAGE",
+			"xLarge@2x": "",
+			"large@2x": "",
+			"timeStamp": 1685602003603
+		}, {
+			"id": 1031856532,
+			"categories": [],
+			"brands": [],
+			"authors": [],
+			"url": "https://www.fairfaxstatic.com.au/content/dam/images/h/2/c/9/l/g/image.related.articleLeadwide.1500x844.p5dcrb.13zzqx.png/1685602003603.jpg",
+			"lastModified": 1685602003603,
+			"sponsored": false,
+			"description": "Builder Alistair Jones has waited 18 months to get approval to convert this freestanding home into a duplex. ",
+			"photographer": "Dominic Lorrimer",
+			"type": "articleLeadWide",
+			"width": 1500,
+			"height": 844,
+			"assetType": "IMAGE",
+			"xLarge@2x": "",
+			"xLarge": "",
+			"large@2x": "",
+			"large": "",
+			"thumbnail@2x": "",
+			"thumbnail": "",
+			"timeStamp": 1685602003603
+		}, {
+			"id": 1031857207,
+			"categories": [],
+			"brands": [],
+			"authors": [],
+			"url": "https://www.fairfaxstatic.com.au/content/dam/images/h/2/c/a/4/7/image.related.wideLandscape.1500x844.p5dcrb.13zzqx.png/1685577464607.jpg",
+			"lastModified": 1685526713807,
+			"sponsored": false,
+			"description": " ",
+			"photographer": "  ",
+			"type": "wideLandscape",
+			"width": 1500,
+			"height": 844,
+			"assetType": "IMAGE",
+			"xLarge@2x": "",
+			"xLarge": "",
+			"large@2x": "",
+			"large": "",
+			"thumbnail@2x": "",
+			"thumbnail": "",
+			"timeStamp": 1685526712516
+		} , {
+			"id": 1031828645,
+			"categories": [],
+			"brands": [],
+			"authors": [],
+			"url": "https://www.fairfaxstatic.com.au/content/dam/images/h/2/b/o/2/t/image.related.landscape.1174x783.p5dce5.13zzqx.png/1685577464607.jpg",
+			"lastModified": 1683252266624,
+			"sponsored": false,
+			"description": "Blockage: The pipeline of new housing shrank further in April.",
+			"photographer": "Tamara Voninski",
+			"type": "landscape",
+			"width": 1174,
+			"height": 783,
+			"assetType": "IMAGE",
+			"large@2x": "",
+			"large": "",
+			"thumbnail@2x": "",
+			"thumbnail": "",
+			"timeStamp": 1683252266624
+		}],
+		"companies": [],
+		"legalStatus": "None",
+		"sources": [{
+			"tagId": "AFR"
+		}],
+		"assetType": "ARTICLE",
+		"overrides": {
+			"overrideHeadline": "Housing pipeline shrinks as apartment approvals plummet",
+			"overrideAbstract": "Economic headwinds are hitting Australia’s largest cities, as they try to boost housing development to overcome a long-standing shortage of homes."
+		},
 		"timeStamp": 1685429492000
-	}],
-	"relatedAssets": [],
-	"relatedImages": [],
-	"assetType": "ASSET_LIST",
-	"timeStamp": 1685577463560
+	}
+]
 } 
 
 
 			}
 		""".trimIndent()
-val assetMock1: Asset = Gson()
-    .fromJson(sampleMock, Asset::class.java)
